6efedb4adaf199da9eb09c77568c14c1860a70ba
Hai Tran <vroyibg@gmail.com>
2021-12-06 23:23:57 +0700

    add the context menu

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+    chrome.storage.sync.set({ color });
+    console.log('Default background color set to %cgreen', `color: ${color}`);
+    chrome.contextMenus.create({
+        id: "create_text_menu",
+        contexts: ["selection"],
+        title: "Create new text in LWT"
+    })
+    chrome.contextMenus.onClicked.addListener((info) => {
+        console.log(info);
+    })
 });
